78a1b97db0b0f09c558e8a26955c4304c6addc68
Fey Daniel <feysy97@gmail.com>
2022-11-18 17:55:29 +0800

    Add controllers

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
+  def show
+    @article_show = Article.find(params[:id])
+  end 
+
+  def new
+    @article_new = Article.new
+  end 
+
+  def create
+    new_article = Article.new(article_params)
+
+    if new_article.save
+      redirect_to new_article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end 
+
+  def edit
+    @article_edit = Article.find(params[:id])
+  end 
+  
+  def update
+    update_article.Article.find(params[:id])
+
+    if update_article.update(article_params)
+      redirect_to update_article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end 
+
+  def destroy
+    destroy_article = Article.find(params[:id])
+    destroy_article.destroy
+
+    redirect_to root_path
+  end 
+
+  private 
+
+  def article_params
+    params.require(:article).permit(:name, :body)
+  end
+
+end
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+  
+  def show
+  end 
+
+  def new
+  end 
+
+  def create
+  end 
+
+  def edit
+  end 
+  
+  def update
+  end 
+
+  def destroy
+  end 
+
+end
