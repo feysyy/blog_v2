36e5ccb0d3d92ed223b3961b0839e68277d47e1c
Fey Daniel <feysy97@gmail.com>
2022-11-26 00:44:41 +0800

    Update models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   validates :name, presence: true,
                    uniqueness: true
-  validates :body,  presence: true,
+  validates :body, presence: true,
                    length: {minimum: 5}
+
+  has_many :comments
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   validates :body, presence: true, length: {minimum: 5, maximum: 500}
 
+  belongs_to :article
 end
