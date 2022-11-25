2ab436105d3aa145374a374fb197f543e80972c9
Fey Daniel <feysy97@gmail.com>
2022-11-26 00:45:37 +0800

    Update migration

---
+++ db/migrate/20221125084616_remove_article_id_from_comments.rb
@@ -0,0 +1,5 @@
+class RemoveArticleIdFromComments < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :comments, :article_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_124155) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_085136) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "body"
@@ -22,6 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_124155) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "article_id", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  add_foreign_key "comments", "articles"
 end
